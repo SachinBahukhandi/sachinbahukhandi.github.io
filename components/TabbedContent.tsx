34578567da6aaf349a775b9bf54fda45821daeec
SachinBahukhandi <sachinb0013@gmail.com>
2024-03-07 10:48:50 +0530

    Documented the template to react

--- components/TabbedContent.tsx
+++ components/TabbedContent.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import About from "./tabs/About";
 import Resume from "./tabs/Resume";
+import Portfolio from "./tabs/Portfolio";
+import Blog from "./tabs/Blog";
+import Contact from "./tabs/Contact";
 type Props = {
     activeTab: Number,
     navItem: String
@@ -13,6 +16,12 @@ const TabbedContent: React.FunctionComponent<Props> = ({ activeTab, navItem }) =
                 navItem == 'about' && <About active={true}/>
                 ||
                 navItem == 'resume' && <Resume active={true}/>
+                ||
+                navItem == 'portfolio' && <Portfolio active={true}/>
+                ||
+                navItem == 'blog' && <Blog active={true}/>
+                ||
+                navItem == 'contact' && <Contact active={true}/>
             }
 
         </>
