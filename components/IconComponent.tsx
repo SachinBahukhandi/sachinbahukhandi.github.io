ee9e7a4cff061e57a5c0fed11619886d76d66387
SachinBahukhandi <sachinb0013@gmail.com>
2024-03-10 22:32:52 +0530

    Fixed the icons dynamic issue

--- components/IconComponent.tsx
+++ components/IconComponent.tsx
@@ -6,14 +6,12 @@ import { IIconComponent } from "../types/IPortfolio";
 type Prop = {
     icon: IIconComponent
 };
-const IconComponent : React.FC<Prop> = ({icon}) => {
-    console.log(icon.color);
-    const IconComponent: any = Icons[icon.key];
-    console.log(IconComponent);
+const IconComponent: React.FC<Prop> = ({ icon }) => {
+    const IconComponent = Icons[`${icon.key}` as keyof typeof Icons];
 
-        if (!IconComponent) { // Return a default one
-            return <Icons.DiCode size={80} color={`${icon.color?? "blue"}`} />;
-          }
-          return <IconComponent size={80} color={`${icon.color?? "blue"}`}/>;
+    if (!IconComponent) { // Return a default one
+        return <Icons.DiCode size={80} color={`${icon.color ?? "blue"}`} />;
+    }
+    return <IconComponent size={80} color={`${icon.color ?? "blue"}`} />;
 };
 export default IconComponent;
